0ee5218354c77c9b2c4149316aa4956d3dde0562
KUBIXQAZ <kubajozwik20027@gmail.com>
2025-06-11 00:59:54 +0200

    Update .NET 9.0

--- Program.cs
+++ Program.cs
@@ -1,17 +1,20 @@
-﻿namespace password_generator
+﻿using System.Text;
+
+namespace PasswordGenerator
 {
-    internal class Program
+    class Program
     {
-        static string generate_password()
+        static string GeneratePassword()
         {
             var upper = new List<string>() { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z" };
             var lower = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z" };
             var numbers = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
-            var special_symbols = new List<string>() { "!", "@", "#", "$", "%", "&" };
-            int password_lenght = 20;
-            var password = new List<string>();
+            var specialSymbols = new List<string>() { "!", "@", "#", "$", "%", "&" };
 
-            for (int i = 0; i < password_lenght; i++)
+            int passwordLength = 20;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < passwordLength; i++)
             {
                 Random random = new Random();
 
@@ -19,25 +22,25 @@
                 switch(x)
                 {
                     case 0:
-                        int random_upper = random.Next(0, upper.Count);
-                        password.Add(upper[random_upper]);
+                        int randomUpper = random.Next(0, upper.Count);
+                        sb.Append(upper[randomUpper]);
                         break;
                     case 1:
-                        int random_lower = random.Next(0, lower.Count);
-                        password.Add(lower[random_lower]);
+                        int randomLower = random.Next(0, lower.Count);
+                        sb.Append(lower[randomLower]);
                         break;
                     case 2:
-                        int random_number = random.Next(0, numbers.Count);
-                        password.Add(numbers[random_number]);
+                        int randomNumber = random.Next(0, numbers.Count);
+                        sb.Append(numbers[randomNumber]);
                         break;
                     case 3:
-                        int random_special_symbol = random.Next(0, special_symbols.Count);
-                        password.Add(special_symbols[random_special_symbol]);
+                        int randomSpecialSymbol = random.Next(0, specialSymbols.Count);
+                        sb.Append(specialSymbols[randomSpecialSymbol]);
                         break;
                 }
             }
 
-            return string.Join("", password);
+            return sb.ToString();
         }
 
         static void Main()
@@ -46,7 +49,7 @@
 
             for (int i = 0; i < passwords; i++)
             {
-                Console.WriteLine("PASSWORD NUMBER " + i + " GENERATED: " + generate_password());
+                Console.WriteLine("PASSWORD NUMBER " + i + " GENERATED: " + GeneratePassword());
             }
 
             Console.WriteLine("GENERATED " + passwords + " PASSWORDS.");
